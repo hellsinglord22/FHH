b507ea847e1768ff5062a4cc9cbcee0918f67e7d
H <heshamscorpe55@gmail>
2016-04-28 06:35:27 +0200

    fix typos

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 var express = require('express'); 
 var http = require('http'); 
 var app = express(); 
-var server = createServer(app).listen(3000); 
+var server = http.createServer(app).listen(3000); 
 var io = require('socket.io')(server); 
 
 // serve the content of the public directory 
-app.use(static.express('public')); 
+app.use(express.static('public')); 
 
 console.log(`listening on port 3000`);
