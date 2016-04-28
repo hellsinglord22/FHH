12d7ac3ff2ca5f0296ae95f89ecb2965f41c1db1
H <heshamscorpe55@gmail>
2016-04-28 06:26:35 +0200

    start serving the content of the public folder

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ var http = require('http');
 var app = express(); 
 var server = createServer(app).listen(3000); 
 var io = require('socket.io')(server); 
+
+// serve the content of the public directory 
+app.use(static.express('public')); 
+
+console.log(`listening on port 3000`);
